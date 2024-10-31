b996150c1798a3f16b93b351afe9449bea51aabd
sabaka-pes <monsutaaisu@gmail.com>
2024-10-31 17:50:51 +0300

    Update ERC20Permit
    Add functionality

--- ERC20Permit.sol
+++ ERC20Permit.sol
@@ -10,6 +10,57 @@ import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 abstract contract ERC20Permit is ERC20, IERC20Permit, EIP712 {
     using Counters for Counters.Counter;
 
+    mapping (address => Counters.Counter) private _nonces;
+
+    bytes32 private constant _PERMIT_TYPEHASH = keccak256(
+        "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+    );
+
+    constructor(string memory name) EIP712(name, "1") {}
+
+    function permit(
+        address owner,
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public virtual {
+        require(block.timestamp <= deadline, "expired deadline");
+
+        bytes32 structHash = keccak256(abi.encode(
+            _PERMIT_TYPEHASH, 
+            owner, 
+            spender, 
+            value, 
+            _useNonce(owner), 
+            deadline
+        ));
+
+        bytes32 hash = _hashTypedDataV4(structHash);
+
+        address signer = ECDSA.recover(hash, v, r, s);
+        require(signer == owner, "invalid signature");
+
+        _approve(owner, spender, value);
+    }
+
+    function nonces(address owner) public view virtual returns (uint) {
+        return _nonces[owner].current();
+    }
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32) {
+        return _domainSeparatorV4();
+    }
+
+    function _useNonce(address owner) internal virtual returns (uint current) {
+        Counters.Counter storage nonce = _nonces[owner];
+        
+        current = nonce.current();
+
+        nonce.increment();
+    }
 }
 
 
